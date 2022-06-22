2248ca17b4ebe9116a87c15fc6401c33cbd13c83
DESKTOP-643D06U\PC <phuc3851@gmail.com>
2022-06-22 16:00:43 +0700

    completed verson 3 22.06.2022

--- employee_information/admin.py
+++ employee_information/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from employee_information.models import Department, Position, Employees
 
 # Register your models here.
-#admin.site.register(Department)
-#admin.site.register(Position)
-#admin.site.register(Employees)
+admin.site.register(Department)
+admin.site.register(Position)
+admin.site.register(Employees)
